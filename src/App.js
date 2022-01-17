ceeb4917be5e6c82b04c353ed970aa763c61d39e
Joao Victor <jov9051@outlook.com>
2022-01-17 18:13:14 -0300

    div cep-container created with h1, input for searching cep, and a submit button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <div className="cep-container">
+                <h1>Buscador CEP</h1>
+                <input type="text" placeholder="Digite seu CEP..." name="cep" />
+                <button type="submit">Procurar</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
